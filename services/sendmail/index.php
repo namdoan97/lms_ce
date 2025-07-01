26da2aaea17d7a22c29c5d07f2b806fb1cbf5b57
Nguyen Tran Ngoc Trung <trungnguyen@ilavietnam.edu.vn>
2025-07-01 16:23:25 +0700

    remove Todd from CC email

--- services/sendmail/index.php
+++ services/sendmail/index.php
@@ -91,7 +91,7 @@ foreach($toList as $key=>$to)
 }
 //$mail->addAddress('thanhtran@ilavietnam.edu.vn', 'Thanh');
 $mail->addCC('ce@ilavietnam.edu.vn', 'CE Group');
-$mail->addCC('todd@ilavietnam.edu.vn', 'Todd');
+//$mail->addCC('todd@ilavietnam.edu.vn', 'Todd');
 //$mail->addCC('anastasiaromashova@ilavietnam.edu.vn', 'Ana');
 //$mail->addCC('trungnguyen@ilavietnam.edu.vn', 'Trung');
 $mail->Subject = $subject;
